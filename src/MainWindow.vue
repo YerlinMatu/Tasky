0de0e833d64df91e09974f1700c10188e01f1a7b
Yerlin Matu <yerlinmatu@MacBook-Pro-de-Yerlin.local>
2018-09-09 11:10:49 -0500

    First scaffolding - task

---
+++ src/MainWindow.vue
@@ -0,0 +1,34 @@
+<template>
+  <Window title="Tasky" width="400" height="500" margined v-on:close="exit">
+    <Box>
+      <Text>Task {{ tasks.length }}</Text>
+        <TextInput label="Write new task..." type="text" v-model="newTask"/>
+      <Button @click="aggTask">Add task</Button>
+      <Separator horizontal/>
+      <Text v-for="(task, item) in tasks" :key="item">
+        {{ task }}
+      </Text>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    newTask: '',
+    tasks: [],
+  }),
+  methods: {
+    aggTask() {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    },
+    deleteTask(item) {
+      this.tasks.splice(item, 1);
+    },
+    exit() {
+      this.$exit();
+    }
+  }
+}
+</script>
